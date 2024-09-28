120266efec373033bba16d5ece397bbcc67a376b
Krzysztof <krzys0100@wp.pl>
2024-09-28 16:40:16 +0200

    Enemy wave spawner

---
+++ Assets/Scripts/EnemySpawnerTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+public class EnemySpawnerTest : MonoBehaviour
+{
+    public event EventHandler OnDeath;
+    public float speed = 5f;
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * speed * Vector3.up);
+    }
+    
+    void Die()
+    {
+        OnDeath?.Invoke(this, EventArgs.Empty);
+        Destroy(gameObject);
+    }
+    
+    [ContextMenu("Invoke Die")]
+    void InvokeDie()
+    {
+        Die();
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public struct Wave
+{
+    public int enemyCount;
+    public float spawnInterval;
+}
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Wave[] waves;
+    public Transform[] spawnPoints;
+    public float timeBetweenWaves = 2f;
+    private int _currentWaveIndex;
+    private int _enemiesRemainingToSpawn;
+    private int _enemiesRemainingAlive;
+    private float _nextSpawnTime;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        while (_currentWaveIndex < waves.Length)
+        {
+            yield return StartCoroutine(SpawnWave(waves[_currentWaveIndex]));
+            _currentWaveIndex++;
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    IEnumerator SpawnWave(Wave wave)
+    {
+        _enemiesRemainingToSpawn = wave.enemyCount;
+        _enemiesRemainingAlive = wave.enemyCount;
+
+        while (_enemiesRemainingToSpawn > 0)
+        {
+            if (Time.time >= _nextSpawnTime)
+            {
+                SpawnEnemy();
+                _enemiesRemainingToSpawn--;
+                _nextSpawnTime = Time.time + wave.spawnInterval;
+            }
+            yield return null;
+        }
+
+        while (_enemiesRemainingAlive > 0)
+        {
+            yield return null;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        GameObject enemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        enemy.GetComponent<EnemySpawnerTest>().OnDeath += OnEnemyDeath;
+    }
+
+    void OnEnemyDeath(object sender, EventArgs e)
+    {
+        _enemiesRemainingAlive--;
+    }
+}
