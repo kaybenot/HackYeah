8f18e01cfb7c8c7dd442dc15f8215f9abbc6b5d1
Kamil Butryn <kaybenot@gmail.com>
2024-09-29 10:44:58 +0200

    implemented winning

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -34,6 +34,13 @@ public class Spawner : MonoBehaviour
             _currentWaveIndex++;
             yield return new WaitForSeconds(5f);
         }
+
+        while (_enemiesRemainingAlive > 0)
+        {
+            yield return new WaitForSeconds(1f);
+        }
+        
+        GameState.Instance.GameWon();
     }
 
     IEnumerator SpawnWave(Wave wave)
