39104a2c94a0d3860ecbf0e801f6d6bac544e1f7
Kamil Butryn <kaybenot@gmail.com>
2024-09-29 10:09:13 +0200

    sounds

--- Assets/Scripts/EnemySpawnable.cs
+++ Assets/Scripts/EnemySpawnable.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 using System;
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Lemurs.Enemies;
+using Random = UnityEngine.Random;
 
 public class EnemySpawnable : MonoBehaviour
 {
     [SerializeField] private EnemyBattler battler;
+    [SerializeField] private List<AudioClip> deathSFX;
     
     public Action OnDeath;
 
     [ContextMenu("Invoke Die")]
     public void Die()
     {
+        if (deathSFX.Count > 0)
+            AudioSource.PlayClipAtPoint(deathSFX[Random.Range(0, deathSFX.Count)], transform.position);
         OnDeath?.Invoke();
         Destroy(gameObject);
     }
