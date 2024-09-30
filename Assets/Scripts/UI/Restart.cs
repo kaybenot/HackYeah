3412953299cf0d5919eb36095b1ace19e59b1ad4
Kosmik <malpa.w.klapkach@gmail.com>
2024-09-30 21:51:09 +0200

    Final commit

--- Assets/Scripts/UI/Restart.cs
+++ Assets/Scripts/UI/Restart.cs
@@ -3,7 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
-    public void OnRestartButtonClick()
+	[SerializeField]
+	private float cooldown = 1;
+	private float timer;
+
+	private void Start()
+	{
+		timer = 0;
+	}
+
+	private void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer < cooldown)
+			return;
+
+		if (Input.anyKeyDown)
+			OnRestartButtonClick();
+	}
+
+	public void OnRestartButtonClick()
     {
         SceneManager.LoadScene(0);
     }
